4e1519b6292b6e4169f7b549f8947f4d3d0638f5
Sofia Cheung <sofiavictoria17@gmail.com>
2020-11-13 14:30:45 -0800

    Creating a homepage component and styling it

---
+++ src/components/Homepage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../styles/homepage.scss'
+
+const HomePage = () => (
+  <section id='homepage'>
+    <div>
+      <h1>Sofía Cheung Guardia</h1>
+      <h2>Full Stack Software Engineer</h2>
+    </div>
+  </section>
+)
+
+export default HomePage
