dd2e339dc82f25ecc2b6652d0033f22832df5b55
Sofía Cheung Guardia <44760650+Svcg17@users.noreply.github.com>
2022-09-01 21:32:29 -0700

    Update SocialIcons.js

--- src/components/SocialIcons.js
+++ src/components/SocialIcons.js
@@ -5,7 +5,6 @@ import LinkedIn from '../assets/LinkedIn.svg'
 
 const SocialIcons = () => (
   <ul className='socialIcons'>
-    <li><a href='https://twitter.com/sofivism'><img src={Twitter} alt='link to Twitter account' /></a></li>
     <li><a href='https://www.linkedin.com/in/sofiacheung/'><img src={LinkedIn} alt='link to LinkedIn account' /></a></li>
     <li><a href='https://github.com/Svcg17'><img src={Github} alt='link to Github account' /></a></li>
   </ul>
