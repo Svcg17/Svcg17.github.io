86703b7e3638a4ae8f8f3728accf4eac0dba6dc0
Sofía Cheung Guardia <44760650+Svcg17@users.noreply.github.com>
2022-09-01 21:29:17 -0700

    Update AboutMe.js

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -5,7 +5,7 @@ const AboutMe = () => (
     <section id='aboutme'>
         <h1>A Bit About Me</h1>
         <div>
-            <p>Welcome to my personal website! My name is Sofía Cheung, born and raised in Panama and currently living in the Bay Area. I attended Holberton School of Software Engineering and specialized in Full-Stack Web Development. Having learned the basics of programming at school and then being able to build complex projects on my own, I can sincerely say that I like what I do. I can't wait to start my career as a Full Stack Software Engineer and be part of a community that values innovation, teamwork and learning. Besides coding, I’m a fan of watercolor art, playing the ukulele and video chatting with my family/friends in Panama.</p>
+            <p>Welcome to my personal website! My name is Sofía Cheung, born and raised in Panama and currently living in the Bay Area. Feel free to view any of my projects on Github.</p>
             <p>Website made with Gatsby and React.</p>
         </div>
     </section>
